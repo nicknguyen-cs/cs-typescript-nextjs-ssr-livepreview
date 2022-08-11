e7292dca35b3c166987038427c17879f81f9dd8e
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2022-08-11 13:55:20 +0530

    content type and entry from ENV

--- sdk/index.tsx
+++ sdk/index.tsx
@@ -3,8 +3,8 @@ import ContentstackLivePreview from "@contentstack/live-preview-utils";
 
 export const Stack = Contentstack.Stack({
     api_key: process.env.API_KEY as string,
-    "delivery_token": process.env.DELIVERY_TOKEN as string,
-    "environment": process.env.ENVIRONMENT as string,
+    delivery_token: process.env.DELIVERY_TOKEN as string,
+    environment: process.env.ENVIRONMENT as string,
     "live_preview": {
         enable: true,
         host: "api.contentstack.io",
@@ -14,6 +14,6 @@ export const Stack = Contentstack.Stack({
 
 
 export async function getEntry() {
-    let entry = await Stack.ContentType('author_2').Entry('bltc7fa3dbf5fd62dea').toJSON().fetch();
+    let entry = await Stack.ContentType(process.env.CONTENT_TYPE as string).Entry(process.env.ENTRY_UID as string).toJSON().fetch();
     return entry;
 }
